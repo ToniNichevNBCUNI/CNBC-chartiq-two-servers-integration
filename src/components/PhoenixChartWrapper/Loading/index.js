4ac0fedbb53301d809aba81754391b0019054a79
ToniNichev <toni.nichev@gmail.com>
2020-03-02 19:52:59 -0500

    #stable added loading

---
+++ src/components/PhoenixChartWrapper/Loading/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const styles = require('./styles.scss');
+
+/**
+ * HELPER COMPONENT TO DISPLAY LOADING ... SCREEN AND HANDLE ERRORS
+ * @param {} props 
+ */
+const Loading = (props) => {
+  if (props.error) {
+    return (<div className={styles.wrapper}>
+              <h3>Error loading component!</h3> 
+              <p><b>{props.error.message}</b></p>
+              <p>{props.error.stack}</p>
+              <div><button onClick={ props.retry }>Retry</button></div>
+            </div>);
+  } else {
+    return (
+    <div className={styles.wrapper}>
+      <div className={styles.timeRange}><p> ... </p></div>
+      <h1>Loading . . . </h1>
+    </div>)
+  }
+} 
+
+export default Loading;
